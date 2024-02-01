d5b167a29d29d62cc11c1328ece9f402bbe3b382
Daniel Lusk <daniel.lusk@posteonet>
2024-02-01 18:36:17 +0000

    add name

--- src/utils/setup_logger.py
+++ src/utils/setup_logger.py
@@ -5,6 +5,6 @@ import logging
 def setup_logger():
     """Setup logging for the project."""
     logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(message)s",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S %Z",
     )

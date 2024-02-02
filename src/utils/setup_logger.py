8dd4c77b1e99efc32ad827c40e130cadf88b5242
Daniel Lusk <daniel.lusk@posteonet>
2024-02-02 16:49:06 +0000

    add subprocess logger util for quick setup in subprocesses

--- src/utils/setup_logger.py
+++ src/utils/setup_logger.py
@@ -8,3 +8,11 @@ def setup_logger():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S %Z",
     )
+
+
+def subprocess_logger() -> logging.Logger:
+    """Setup a logger for a subprocess."""
+    setup_logger()
+    subp_log = logging.getLogger("__main__")
+    subp_log.setLevel(logging.INFO)
+    return subp_log
